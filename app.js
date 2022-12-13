5a8bb9d487b58bf3ea31684d46e970329bd49557
Masson <35528949+MassonNN@users.noreply.github.com>
2022-12-13 16:31:00 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
+let item = "";
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.show();
+    tg.sendData("sendTestMessage");
+});
